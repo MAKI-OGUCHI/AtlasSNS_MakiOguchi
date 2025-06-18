a233aaf96354ad9c1d123196b54652852c5f3a3d
MAKI-OGUCHI <maoguchi1126@gmail.com>
2025-06-18 16:45:10 +0900

    ページに遷移するリンク設置

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -28,7 +28,7 @@
   <header>
     @include('layouts.navigation')
     <div id="head">
-      <h1><a href="/top"><img src="images/atlas.png" alt=""></a></h1>
+      <!-- <h1><a href="/top"><img src="images/atlas.png" alt=""></a></h1> -->
       <div>
         <div>
           <p>{{session('user')}}<img src=>
@@ -40,7 +40,7 @@
   <!-- Page Content -->
   <div id="row">
     <div id="container">
-      <!-- {$slot}@yield('content') -->
+      {{$slot}}
     </div>
     <div id="side-bar">
       <div id="confirm">
@@ -49,14 +49,14 @@
           <p>フォロー数</p>
           <p>〇〇名</p>
         </div>
-        <p class="btn"><a href="">フォローリスト</a></p>
+        <p class="btn"><a href="/follow-list">フォローリスト</a></p>
         <div>
           <p>フォロワー数</p>
           <p>〇〇名</p>
         </div>
-        <p class="btn"><a href="">フォロワーリスト</a></p>
+        <p class="btn"><a href="/follower-list">フォロワーリスト</a></p>
       </div>
-      <p class="btn"><a href="">ユーザー検索</a></p>
+      <p class="btn"><a href="/search">ユーザー検索</a></p>
     </div>
   </div>
   <footer>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\FollowsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +26,9 @@ Route::get('top', [PostsController::class, 'index']);
 
 Route::get('profile', [ProfileController::class, 'profile']);
 
-Route::get('search', [UsersController::class, 'index']);
+Route::get('search', [UsersController::class, 'users']);
 
-Route::get('follow-list', [PostsController::class, 'index']);
-Route::get('follower-list', [PostsController::class, 'index']);
+Route::get('follow-list', [FollowsController::class, 'followList']);
+Route::get('follower-list', [FollowsController::class, 'followerList']);
+
+Route::get('logout', [AuthenticatedSessionController::class, 'create']);
